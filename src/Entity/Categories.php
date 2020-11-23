58b0fa71d95a364243b9762f793db42448a644f5
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-23 14:29:38 +0100

    entité Jobs ok

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Categories
      */
     private $nom;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Jobs::class, mappedBy="category")
+     */
+    private $jobs;
+
+    public function __construct()
+    {
+        $this->jobs = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Categories
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Jobs[]
+     */
+    public function getJobs(): Collection
+    {
+        return $this->jobs;
+    }
+
+    public function addJob(Jobs $job): self
+    {
+        if (!$this->jobs->contains($job)) {
+            $this->jobs[] = $job;
+            $job->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJob(Jobs $job): self
+    {
+        if ($this->jobs->removeElement($job)) {
+            // set the owning side to null (unless already changed)
+            if ($job->getCategory() === $this) {
+                $job->setCategory(null);
+            }
+        }
 
         return $this;
     }
