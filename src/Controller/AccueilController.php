1e6affce53938be75ce74fdb1dfc627157c97cc9
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-24 17:07:57 +0100

    img et réglages accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,22 +2,23 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\JobsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
-{
+class AccueilController extends AbstractController {
     /**
-     * @Route("/", name="accueil")
+     * @const JOBS_PAGE nombre de jobs maximal à afficher
      */
     const JOBS_PAGE = 10;
 
-    public function index(EntityManagerInterface $em): Response
-    {   
-        $repo = $em->getRepository(Jobs::class);
-        $jobs = $repo-> findBy(['active'=>'true'],['update'=>'DESC','category'=>'ASC']);
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(JobsRepository $jR): Response
+    {
+        $jobs = $jR-> findBy(['active' => 1],['updated'=>'DESC','category'=>'ASC'],self::JOBS_PAGE,0);
 
         return $this->render('accueil/index.html.twig', [
             'jobs' => $jobs,
