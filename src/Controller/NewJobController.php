13b2c80e12fb46e7b529ca04480cc5a73c49a55b
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-26 22:43:20 +0100

    Manque un élément pour le form de modif d'annonce

--- src/Controller/NewJobController.php
+++ src/Controller/NewJobController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Jobs;
 use App\Form\NewJobType;
 use App\Service\FileUploader;
+use App\Repository\JobsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,27 +29,128 @@ class NewJobController extends AbstractController
             $file = $form['logo']->getData();
             if ($file) {
                 $file_name = $fileUploader->upload($file);
-                if ($file_name !== null) {
-                    $directory = $fileUploader->getTargetDirectory();
-                    $full_path = $directory.'/'.$file_name;
+                if ($file_name === null) {
+                    throw $this->createNotFoundException("un souci avec votre fichier"); 
                 }
             }
             $job = $form->getData();
+            $job->setLogo($file_name);
             $job->setActive(1);
             $job->setCreated(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
             $job->setExpire(date_add(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')), date_interval_create_from_date_string('30 days')));
             $job->setUpdated(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
-            $job->setToken();
-            // ... perform some action, such as saving the task to the database
-            // for example, if Jobs is a Doctrine entity, save it!
+            $job->setToken(bin2hex(random_bytes(32)));
+
             $em->persist($job);
             $em->flush();
 
-            return $this->redirectToRoute('accueil');
+            return $this->redirectToRoute('lienmodif', ['id' => $job->getId()]);
         }
 
         return $this->render('new_job/index.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/lienmodif{id}", name="lienmodif")
+     */
+    public function lien($id, JobsRepository $jr)
+    {
+        $token = $jr->find($id);
+
+        if (!$token) {             
+            throw $this->createNotFoundException("Je ne trouve pas votre annonce");         
+        }
+
+        return $this->render('new_job/link.html.twig', [
+            'token' => $token->getToken(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/update/annonce/{token}", name="update")
+     */
+    public function update($token, JobsRepository $jr, EntityManagerInterface $em, Request $request, FileUploader $fileUploader)
+    {
+        // On recherche si un job avec ce token existe dans la base de données
+        $job = $jr->findOneBy(['token' => $token]);
+        
+        // Récupérer la différence en jours entre maintenant et la fin d'activité de l'annonce
+        /**
+         * @param DateTime $oDate1
+         * @param DateTime $oDate2
+         * @return array 
+         */
+        function date_diff_array(\DateTime $oDate1, \DateTime $oDate2) {
+            $aIntervals = array(
+                'year'   => 0,
+                'month'  => 0,
+                'week'   => 0,
+                'day'    => 0,
+                'hour'   => 0,
+                'minute' => 0,
+                'second' => 0,
+            );
+
+            foreach($aIntervals as $sInterval => &$iInterval) {
+                while($oDate1 <= $oDate2){ 
+                    $oDate1->modify('+1 ' . $sInterval);
+                    if ($oDate1 > $oDate2) {
+                        $oDate1->modify('-1 ' . $sInterval);
+                        break;
+                    } else {
+                        $iInterval++;
+                    }
+                }
+            }
+
+            return $aIntervals;
+        }
+        $diff = date_diff_array(new \DateTime(), $job->getExpire());
+
+        // Si aucun utilisateur n'est associé à ce token
+        if(!$job){
+            // On renvoie une erreur 404
+            throw $this->createNotFoundException("Cette annonce n'existe pas");
+        } else if($diff['week'] < 1 &&$diff['day'] < 5 ) {//ou si il reste moins de 5 jours avnt la fin de validité de l'annonce
+            // On renvoie une erreur 404
+            throw $this->createNotFoundException("Cette annonce n'est plus modifiable");
+        }
+
+
+        //permettre de modifier l'annonce
+        $formulModif = $this->createForm(NewJobType::class, $job);
+
+        $formulModif->handleRequest($request);
+        if ($formulModif->isSubmitted() && $formulModif->isValid()) {
+            // gestion d'upload de l'image
+            $file = $formulModif['logo']->getData();
+            if ($file) {
+                $file_name = $fileUploader->upload($file);
+                if ($file_name === null) {
+                    throw $this->createNotFoundException("un souci avec votre fichier"); 
+                }
+            }
+            $job = $formulModif->getData();
+            $job->setLogo($file_name);
+            $job->setActive(1);
+            $job->setExpire(date_add(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')), date_interval_create_from_date_string('30 days')));
+            $job->setUpdated(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+            // Renvoie les modifs dans la base
+            $em->persist($job);
+            $em->flush();
+            // On génère un message
+            $this->addFlash('message', 'Annonce modifiée avec succès');
+            
+            // On retourne à l'accueil
+            return $this->redirectToRoute('accueil');
+        }
+        return $this->render('new_job/modif.html.twig', [
+            'form' => $formulModif->createView(),
+            'job' => $job
+        ]);
+    }
+
 }
