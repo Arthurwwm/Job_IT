007f049729aedd4d429bbc52b079418702ca5fe8
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-26 10:19:29 +0100

    en cours de formulaire (type et controller)

---
+++ src/Controller/NewJobController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Jobs;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class NewJobController extends AbstractController
+{
+    /**
+     * @Route("/new/job", name="new_job")
+     */
+    public function new(Request $request): Response
+    {
+        $job = new Jobs();
+        $job->setActive();
+        $job->setCreated();
+        $job->setUpdated();
+        $job->setCreated();
+        $job->setToken();
+
+        $form = $this->createForm(NewJobType::class, $job);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $job = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($job);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('new_job/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
--- src/Form/NewJobType.php
+++ src/Form/NewJobType.php
@@ -6,28 +6,43 @@ use App\Entity\Jobs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NewJobType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contrat')
-            ->add('entreprise')
+            ->add('intitule', TextType::class,[
+                'label' => 'Intitulé du poste',
+            ])
+            ->add('contrat', TextType::class,[
+                'label' => 'Type de Contrat',
+                'placeholder' => 'CDI/CDD...'
+            ])
+            ->add('category', ChoiceType::class,)
+            ->add('entreprise', TextType::class,[
+                'label' => 'Votre Entreprise',
+                'placeholder' => 'Google ?'
+            ])
             ->add('logo')
-            ->add('url')
-            ->add('pays')
-            ->add('lieu')
-            ->add('description')
-            ->add('token')
+            ->add('url', TextType::class)
+            ->add('lieu', TextType::class,[
+                'label' => 'Ville',
+                ])
+            ->add('pays', TextType::class,[
+                'label' => 'Type de Contrat',
+                'placeholder' => 'CDI/CDD...'
+            ])
+            ->add('description', TextareaType::class)
             ->add('email')
-            ->add('active')
+            ->add('postuler')
+
             ->add('expire')
             ->add('created')
             ->add('updated')
-            ->add('postuler')
-            ->add('intitule')
-            ->add('category')
         ;
     }
 
